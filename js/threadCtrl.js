863d3021eea8f6ce7674318f8ecb69c3afddf125
Brian Kemper <bkemper@gmail.com>
2016-02-24 15:49:27 -0700

    done

--- js/threadCtrl.js
+++ js/threadCtrl.js
@@ -1,11 +1,17 @@
 angular.module('app').controller('threadCtrl', function($scope, $firebaseObject, $firebaseArray, threadSvc, threadRef, commentsRef){
 
   var thread = $firebaseObject(threadRef);
-  
+
   thread.$bindTo($scope, 'thread');
 
-$scope.createThread = function () {
+  $scope.createThread = function () {
     $scope.threads.$add($scope.newThread);
+  };
+
+  $scope.comments = $firebaseArray(commentsRef);
+
+  $scope.createComment = function(){
+    $scope.comments.$add($scope.newComment);
   };
 
 });
